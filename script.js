5e1ea3f26f2875f34d828cbc46ba103260f8d455
PourquoiEstTu <rsquyum@gmail.com>
2023-08-22 16:25:15 -0400

    Add condition for addBtn func and edit clearBtn
    addbtn no longer does anything when the display is empty
    or when addition has already been pressed without another
    number being pressed after.
    
    result and skip variables moved up so clearBtn can access
    and reset them to 0 when clicked.

--- script.js
+++ script.js
@@ -12,17 +12,24 @@ numBtns.forEach(btn => btn.addEventListener("click",
         else display.value += btn.textContent;
     }));
 
+let result = 0;
+let skip = 0;
+
 // clear display
 const clearBtn = document.querySelector("#clear");
-clearBtn.addEventListener("click", () => display.value = "");
+clearBtn.addEventListener("click", () => {
+    display.value = "";
+    result = 0;
+    skip = 0;
+});
 
-let result;
-let skip = 0;
 const addBtn = document.querySelector("#add");
 addBtn.addEventListener("click", () => {
+    if (display.value === "" || display.value.charAt(display.value.length - 1) === " ") return;
     display.value += " " + addBtn.textContent + " ";
     let numOfDigits = display.value.indexOf(" ", skip);
-    result = parseInt(display.value.slice(skip, numOfDigits));
-    skip += numOfDigits + 2;
+    result += parseInt(display.value.slice(skip, numOfDigits));
+    skip += numOfDigits + 3;
+    // console.log("result = " + result);
+    // console.log("skip = " + skip);
 });
-console.log(result);
